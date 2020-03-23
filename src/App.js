4769f123a9d97cafa463b9957678c2a5074a1020
Nick <nearwood@gmail.com>
2020-03-23 16:30:06 -0500

    Fix decrypt

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const iv = "\x31\x32\x33\x34\x35\x36\x37\x38\x62\x30\x7a\x32\x33\x34\x35\x6e";
+
 function App() {
   const [input, setInput] = useState("6k3teSVVjbc6AxRpghGZ6Zxq2igsO6ZTaw+7tC9y7pSWObcZBydHsuuykMaOtr/xKSD8uA7lVkurGPkv7vK469AOkWkJBS2IuiMU8ploCziWvffkhyiSS2MRUEp9aRlH/9gdGuR5tAfMZEz4zHjqkt0eAfDe6y8yozwYWxIHSMzYHzFe37e5FaA/XXYXBQEz5/i9SKSNJsms002OrJCljVcHl4jCD7m1y3o7hBuaonGKpc9iY3+s3DIdqRJ8nP2Fa5cJx/IkDoiVvsyAVkq/Aue3pd6Fub4uzB5nambt0JVYAIO+GORdRwiLiuDIA4EvrKLZYv6z8CKkHdqjfC8KJmTRFwa+kwm2pLuUwaLIGSI=");
   const [key, setKey] = useState("");
+  const [output, setOutput] = useState("");
 
   return (
     <div className="App">
       <textarea rows="8" cols="80" placeholder="input" value={input} onChange={(e) => setInput(e.target.value)}/>
-      <input type="text" size="72" placeholder="key" value={key} onChange={(e) => setKey(e.target.value)}/>
-      <div id="buttons"><button onClick={() => encrypt(key, input)}>Encrypt</button> <button onClick={() => decrypt(key, input)}>Decrypt</button></div>
+      <input type="text" size="72" placeholder="key" maxLength="32" value={key} onChange={(e) => setKey(e.target.value)}/>
+      <div id="buttons"><button onClick={() => encrypt(key, input, setOutput)}>Encrypt</button> <button onClick={() => decrypt(key, input, setOutput)}>Decrypt</button></div>
       <div id="errors"></div>
-      <textarea rows="8" cols="80" placeholder="output"/>
+      <textarea rows="8" cols="80" placeholder="output" value={output} readOnly/>
       <pre>{blah()}</pre>
     </div>
   );
 }
 
-function encrypt(keyString, plaintext) {
+function encrypt(keyString, plaintext, setOutput) {
   initCrypto(keyString, (key) => {
     encryptMessage(key, plaintext, (ciphertext) => {
-      console.log(ciphertext);
+      setOutput(ciphertext);
     });
   });
 }
 
-function decrypt(keyString, ciphertext) {
+function decrypt(keyString, ciphertext, setOutput) {
   initCrypto(keyString, (key) => {
     decryptMessage(key, ciphertext, (plaintext) => {
-      console.log(plaintext);
+      setOutput(plaintext);
     });
   });
 }
@@ -65,30 +68,30 @@ function encryptMessage(key, plaintext, next) {
   window.crypto.subtle.decrypt(
     {
       name: "AES-CBC",
-      iv: stringToArrayBuffer('\x31\x32\x33\x34\x35\x36\x37\x38\x62\x30\x7a\x32\x33\x34\x35\x6e')
+      iv: stringToArrayBuffer(iv) //Uint8Array.from(iv) //?
     },
     key,
     stringToArrayBuffer(plaintext)
   ).then((ciphertext) => {
     next(ciphertext);
-  }).catch(err => console.error(err.name));
+  }).catch(err => console.error(err.name, err));
 }
 
 function decryptMessage(key, ciphertext, next) {
   window.crypto.subtle.decrypt(
     {
       name: "AES-CBC",
-      iv: stringToArrayBuffer('\x31\x32\x33\x34\x35\x36\x37\x38\x62\x30\x7a\x32\x33\x34\x35\x6e')
+      iv: stringToArrayBuffer(iv)
     },
     key,
     base64ToArrayBuffer(ciphertext)
   ).then((plaintext) => {
-    next(plaintext);
-  }).catch(err => console.error(err.name));
+    next(arrayBufferToString(plaintext));
+  }).catch(err => console.error(err.name, err));
 }
 
 function initCrypto(rawKey, next) {
-  window.crypto.subtle.importKey("raw", stringToArrayBuffer(rawKey.padStart(32, '\0')), { name: "AES-CBC", length: 256 }, true, ["encrypt", "decrypt"])
+  window.crypto.subtle.importKey("raw", stringToArrayBuffer(rawKey.padEnd(32, '\0')), { name: "AES-CBC", length: 256 }, true, ["encrypt", "decrypt"])
     .then((key) => {
       next(key);
     }).catch(err => console.error(err.name));
@@ -106,6 +109,16 @@ function stringToArrayBuffer(s) {
 function base64ToArrayBuffer(b64) {
   var byteString = window.atob(b64);
   return stringToArrayBuffer(byteString);
+}
+
+function arrayBufferToString(buffer) {
+  let s = '';
+  let bytes = new Uint8Array(buffer);
+  for (let i = 0; i < bytes.byteLength; ++i) {
+    s += String.fromCharCode(bytes[i]);
+  }
+
+  return s;
 }
 
 export default App;
