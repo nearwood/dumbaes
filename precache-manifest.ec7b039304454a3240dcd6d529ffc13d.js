5c4a692a6001d6b1cad3fc3289914e5a3064345f
Nick <nearwood@gmail.com>
2020-03-23 21:51:26 -0500

    Updates

---
+++ precache-manifest.ec7b039304454a3240dcd6d529ffc13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453cb4ceed4054f3b01cf07cc591d289",
+    "url": "/dumbaes/index.html"
+  },
+  {
+    "revision": "64f8bbd80cba10744cf8",
+    "url": "/dumbaes/static/css/main.ad3f32ce.chunk.css"
+  },
+  {
+    "revision": "61e6d19b7f6da91ff820",
+    "url": "/dumbaes/static/js/2.0ba3ce44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dumbaes/static/js/2.0ba3ce44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f8bbd80cba10744cf8",
+    "url": "/dumbaes/static/js/main.49c74494.chunk.js"
+  },
+  {
+    "revision": "6cd634aa8489d7cbda8c",
+    "url": "/dumbaes/static/js/runtime-main.c6687a68.js"
+  }
+]);
