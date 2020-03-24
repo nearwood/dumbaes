d3cb286e9224ada5664ed1aa0235b4adedd50689
Nick <nearwood@gmail.com>
2020-03-23 21:46:14 -0500

    Styling and add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
-
 import React, { useState } from 'react';
 import './App.css';
 
-//This is terrible in more one way way.
+const VERSION = process.env.REACT_APP_GIT_COMMIT_HASH || 'dev';
+
+//This is terrible in more than one way.
 const iv = "\x31\x32\x33\x34\x35\x36\x37\x38\x62\x30\x7a\x32\x33\x34\x35\x6e";
 
 const utf8Encoder = new TextEncoder();
@@ -18,10 +18,11 @@ export default function App() {
   return (
     <div className="App">
       <textarea rows="8" cols="80" placeholder="input" value={input} onChange={(e) => setInput(e.target.value)}/>
-      <input type="text" size="72" placeholder="key" maxLength="32" value={key} onChange={(e) => setKey(e.target.value)}/>
-      <div id="buttons"><button onClick={() => encrypt(key, input, setOutput, setError)}>Encrypt</button><div id="errors">{error}</div><button onClick={() => decrypt(key, input, setOutput, setError)}>Decrypt</button></div>
+      <div id="buttons"><input id="key" type="text" placeholder="key" maxLength="32" value={key} onChange={(e) => setKey(e.target.value)}/><button onClick={() => encrypt(key, input, setOutput, setError)}>Encrypt</button><button onClick={() => decrypt(key, input, setOutput, setError)}>Decrypt</button></div>
+      <div id="errors">{error}</div>
       <textarea rows="8" cols="80" placeholder="output" value={output} readOnly/>
-      <pre>{blah()}</pre>
+      {/*<pre>{blah()}</pre>*/}
+      <footer><span title="version">{VERSION.substring(0, 7)}</span></footer>
     </div>
   );
 }
@@ -34,7 +35,7 @@ async function encrypt(keyString, plaintext, setOutput, setError) {
     setOutput(ciphertext);
   } catch (e) {
     console.error(e);
-    setError("Could not encrypt.");
+    setError("Error: Could not encrypt.");
   }
 }
 
@@ -46,36 +47,36 @@ async function decrypt(keyString, ciphertext, setOutput, setError) {
     setOutput(plaintext);
   } catch (e) {
     console.error(e);
-    setError("Could not decrypt.");
+    setError("Error: Could not decrypt.");
   }
 }
 
-function blah() {
-  let r = "";
-  //var p3 = '186';
-  var p3 = '054 056'; //'049 054 056'
-  //var p3 = '049 056 054';
+// function blah() {
+//   let r = "";
+//   //var p3 = '186';
+//   var p3 = '054 056'; //'049 054 056'
+//   //var p3 = '049 056 054';
 
-  //var p2 = '087 104 101 110 032 116 104 101';
-  var p2 = '087 104 110 032 116 104';
-  //var p1 = "When the Moon will shed no light With the Equinox close by In the dark a spellware rise Riding on a lightning strike Made of circuit boards and bytes Forged to cross our space and time Thru the storm my soul will guide To the path for glory and might Now just take some of your time While we share this ritual night There's a secret gift that hides But won't come without a fight First you need to get the rhyme When here comes the darkest night Then you take the first 8 bytes Count the pins and normalize Once you got the numbers right Just beware the odds are lies Line them in a row and write That will put the code in sight Now don't stress and take your time Sometimes words can trick your mind For in ASCII code you'll find The solution to these rhymes";
-  var p1 = "light by byte rise strike bytes time guide might time night hides fight rhyme night bytes normalize right lies write sight time mind find rhymes";
+//   //var p2 = '087 104 101 110 032 116 104 101';
+//   var p2 = '087 104 110 032 116 104';
+//   //var p1 = "When the Moon will shed no light With the Equinox close by In the dark a spellware rise Riding on a lightning strike Made of circuit boards and bytes Forged to cross our space and time Thru the storm my soul will guide To the path for glory and might Now just take some of your time While we share this ritual night There's a secret gift that hides But won't come without a fight First you need to get the rhyme When here comes the darkest night Then you take the first 8 bytes Count the pins and normalize Once you got the numbers right Just beware the odds are lies Line them in a row and write That will put the code in sight Now don't stress and take your time Sometimes words can trick your mind For in ASCII code you'll find The solution to these rhymes";
+//   var p1 = "light by byte rise strike bytes time guide might time night hides fight rhyme night bytes normalize right lies write sight time mind find rhymes";
 
-  p1 = p1.split(' ');
+//   p1 = p1.split(' ');
 
-  p1.forEach(word => {
-    var code = word.split('').map(c => c.charCodeAt(0) % 2 === 0 ? ('' + c.charCodeAt(0)).padStart(3, '0') : null).join('');
-    //var code = word.split('').map(c => ('' + c.charCodeAt(0)).padStart(3, '0')).join('');
-    code += p2.split(' ').join('');
-    code += p3.split(' ').join('');
-    //if (code.length >= 30) {
-      //code = code.replace(/[1,3,5,7,9]+/g,'');
-      r += `${word} (${code.length}): ` + code + "\n";
-    //}
-  });
+//   p1.forEach(word => {
+//     var code = word.split('').map(c => c.charCodeAt(0) % 2 === 0 ? ('' + c.charCodeAt(0)).padStart(3, '0') : null).join('');
+//     //var code = word.split('').map(c => ('' + c.charCodeAt(0)).padStart(3, '0')).join('');
+//     code += p2.split(' ').join('');
+//     code += p3.split(' ').join('');
+//     //if (code.length >= 30) {
+//       //code = code.replace(/[1,3,5,7,9]+/g,'');
+//       r += `${word} (${code.length}): ` + code + "\n";
+//     //}
+//   });
 
-  return r;
-}
+//   return r;
+// }
 
 async function encryptMessage(key, plaintext) {
   return arrayBufferToBase64(await window.crypto.subtle.encrypt(
