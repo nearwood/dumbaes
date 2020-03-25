450e08e6ebf1aeb4ea3b22656b1170209df27cca
Nick <nearwood@gmail.com>
2020-03-24 20:36:13 -0500

    some better styling and mv ctf code

--- src/App.js
+++ src/App.js
@@ -17,11 +17,10 @@ export default function App() {
 
   return (
     <div className="App">
-      <textarea rows="8" cols="80" placeholder="input" value={input} onChange={(e) => setInput(e.target.value)}/>
-      <div id="buttons"><input id="key" type="text" placeholder="key" maxLength="32" value={key} onChange={(e) => setKey(e.target.value)}/><button onClick={() => encrypt(key, input, setOutput, setError)}>Encrypt</button><button onClick={() => decrypt(key, input, setOutput, setError)}>Decrypt</button></div>
+      <textarea rows="8" cols="80" placeholder="Input" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <div id="buttons"><input id="key" type="text" placeholder="Key" maxLength="32" value={key} onChange={(e) => setKey(e.target.value)}/><button onClick={() => encrypt(key, input, setOutput, setError)}>Encrypt</button><button onClick={() => decrypt(key, input, setOutput, setError)}>Decrypt</button></div>
       <div id="errors">{error}</div>
-      <textarea rows="8" cols="80" placeholder="output" value={output} readOnly/>
-      {/*<pre>{blah()}</pre>*/}
+      <textarea rows="8" cols="80" placeholder="Output" value={output} readOnly/>
       <footer><a rel="author" href="https://twitter.com/nearwood">@nearwood</a> <a href="https://github.com/nearwood/dumbaes">GitHub</a> <span title="version">{VERSION.substring(0, 7)}</span></footer>
     </div>
   );
@@ -50,33 +49,6 @@ async function decrypt(keyString, ciphertext, setOutput, setError) {
     setError("Error: Could not decrypt.");
   }
 }
-
-// function blah() {
-//   let r = "";
-//   //var p3 = '186';
-//   var p3 = '054 056'; //'049 054 056'
-//   //var p3 = '049 056 054';
-
-//   //var p2 = '087 104 101 110 032 116 104 101';
-//   var p2 = '087 104 110 032 116 104';
-//   //var p1 = "When the Moon will shed no light With the Equinox close by In the dark a spellware rise Riding on a lightning strike Made of circuit boards and bytes Forged to cross our space and time Thru the storm my soul will guide To the path for glory and might Now just take some of your time While we share this ritual night There's a secret gift that hides But won't come without a fight First you need to get the rhyme When here comes the darkest night Then you take the first 8 bytes Count the pins and normalize Once you got the numbers right Just beware the odds are lies Line them in a row and write That will put the code in sight Now don't stress and take your time Sometimes words can trick your mind For in ASCII code you'll find The solution to these rhymes";
-//   var p1 = "light by byte rise strike bytes time guide might time night hides fight rhyme night bytes normalize right lies write sight time mind find rhymes";
-
-//   p1 = p1.split(' ');
-
-//   p1.forEach(word => {
-//     var code = word.split('').map(c => c.charCodeAt(0) % 2 === 0 ? ('' + c.charCodeAt(0)).padStart(3, '0') : null).join('');
-//     //var code = word.split('').map(c => ('' + c.charCodeAt(0)).padStart(3, '0')).join('');
-//     code += p2.split(' ').join('');
-//     code += p3.split(' ').join('');
-//     //if (code.length >= 30) {
-//       //code = code.replace(/[1,3,5,7,9]+/g,'');
-//       r += `${word} (${code.length}): ` + code + "\n";
-//     //}
-//   });
-
-//   return r;
-// }
 
 async function encryptMessage(key, plaintext) {
   return arrayBufferToBase64(await window.crypto.subtle.encrypt(
